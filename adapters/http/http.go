5e49c148b72dea64e4f9e267a485315d2361025e
Christian Beedgen <raychaser@gmail.com>
2015-04-12 15:52:50 -0700

    Structured message format - JSON

--- adapters/http/http.go
+++ adapters/http/http.go
@@ -2,12 +2,14 @@ package http
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gliderlabs/logspout/router"
 )
@@ -58,9 +60,22 @@ func NewHTTPAdapter(route *router.Route) (router.LogAdapter, error) {
 // Stream implements the router.LogAdapter interface.
 func (a *HTTPAdapter) Stream(logstream chan *router.Message) {
 
-	for message := range logstream {
-		message := bytes.NewBufferString(message.Data)
-		req, err := http.NewRequest("POST", a.url, message)
+	for m := range logstream {
+		//		message := bytes.NewBufferString(message.Data)
+
+		// Create a JSON representation of the log message
+		httpMessage := HTTPMessage{
+			Message:  m.Data,
+			Time:     m.Time.Format(time.ISO8601),
+			Source:   m.Source,
+			Name:     m.Container.Name,
+			ID:       m.Container.ID,
+			Image:    m.Container.Config.Image,
+			Hostname: m.Container.Config.Hostname,
+		}
+		message, err := json.Marshal(httpMessage)
+
+		req, err := http.NewRequest("POST", a.url, bytes.NewReader(message))
 		if err != nil {
 			log.Println("syslog:", err)
 			return
@@ -73,4 +88,15 @@ func (a *HTTPAdapter) Stream(logstream chan *router.Message) {
 		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
 	}
+}
+
+// HTTPMessage is a simple JSON representation of the log message.
+type HTTPMessage struct {
+	Message  string `json:"message"`
+	Time     string `json:"time"`
+	Source   string `json:"source"`
+	Name     string `json:"docker.name"`
+	ID       string `json:"docker.id"`
+	Image    string `json:"docker.image"`
+	Hostname string `json:"docker.hostname"`
 }
